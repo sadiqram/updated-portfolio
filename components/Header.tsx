a7a302bcfb6908eeb278601e54ebe1f1e60b1bcf
Sadiq Ramanu <abdulrahmansadiq13@gmail.com>
2025-04-01 14:38:51 -0400

    chore: add some components including header

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import { Button } from './ui/button';
+
+export default function Header() {
+  return (
+    <header>
+        <div className="py-8 xl:py-12 text-white">header</div>
+    </header>
+  );
+}
+
+
