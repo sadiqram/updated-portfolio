f5301e7dc061863fd978ca614774b906269c7bd6
Sadiq Ramanu <abdulrahmansadiq13@gmail.com>
2025-04-19 22:25:31 -0400

    feat: add footer to main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,50 +6,49 @@ import Skills from "@/app/skills/page"
 import Projects from "@/app/projects/page"
 import Experience from "@/app/Experience/page"
 import Contact from "@/app/contact/page"
+import Footer from "@/components/Footer"
+
+
+
+
+
+
 export default function Home() {
   return(
-    
     <section className="h-full">
-   
-    <div className="container mx-auto px-4 xl:px-0">
-   
-      <div className="flex flex-col items-center justify-center xl:flex-row xl:pt-8 xl:pb-24 ">
-     
-        <div className="text-center xl:text-left order-2 xl:order-none">
-        
-        <span className="text-xl">Software Engineer</span>
-        <h1 className="h1 mb-6"> Hi,I&apos;m <br /><span className="text-[#ff004f]">Sadiq</span>  </h1>
-        <p className="max-w-[500px] mb-9 text-[var(--color-text-muted)] transition-colors duration-300">
-          I&apos;m a software engineer with a passion for building scalable and efficient systems. I&apos;m currently NOT working as a software engineer at <span className="text-color-primary">Google</span> and <span className="text-color-primary">Facebook</span>.
-        </p>
-        {/* social media icons */}
-        <div className="flex flex-col xl:flex-row items-center gap-8">
-          <Button variant="outline" size="lg" className="flex items-center gap-2 border border-[#ff004f] text-[#ff004f] hover:bg-[#ff004f] hover:text-white transition-colors duration-300">
-            <span>Download Resume</span>
-            <FiDownload className="text-xl" />
-          </Button>
-          <div className="mb-8 xl:mb-0">
-            <Social  containerStyles="flex gap-6" iconStyles = "w-9 h-9 border border-[#ff004f] rounded-full flex items-center justify-center hover:bg-[#ff004f] hover:text-white hover:transition-all duration-500"/>
-
+      <div className="container mx-auto px-4 xl:px-0">
+        <div className="flex flex-col items-center justify-center xl:flex-row xl:pt-4 xl:pb-12">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Software Engineer</span>
+            <h1 className="h1 mb-4"> Hi,I&apos;m <br /><span className="text-[#ff004f]">Sadiq</span>  </h1>
+            <p className="max-w-[500px] mb-6 text-[var(--color-text-muted)] transition-colors duration-300">
+              I&apos;m a software engineer with a passion for building scalable and efficient systems. I&apos;m currently NOT working as a software engineer at <span className="text-color-primary">Google</span> and <span className="text-color-primary">Facebook</span>.
+            </p>
+            {/* social media icons */}
+            <div className="flex flex-col xl:flex-row items-center gap-6">
+              <Button variant="outline" size="lg" className="flex items-center gap-2 border border-[#ff004f] text-[#ff004f] hover:bg-[#ff004f] hover:text-white transition-colors duration-300">
+                <span>Download Resume</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-4 xl:mb-0">
+                <Social  containerStyles="flex gap-6" iconStyles = "w-9 h-9 border border-[#ff004f] rounded-full flex items-center justify-center hover:bg-[#ff004f] hover:text-white hover:transition-all duration-500"/>
+              </div>
+            </div>
+          </div>
+          <div className="order-1 xl:order-none mb-6 xl:mb-0">
+            <Photo />
           </div>
         </div>
-        </div>
-        <div className="order-1 xl:order-none mb-8 xl:mb-0">
-          <Photo />
-        </div>
       </div>
-      
-    </div>
-    {/* skills section */}
-    <Skills />
-    {/* Experience and Qualifications section */}
-    <Experience />
-    {/* projects section */}
-    <Projects />
+      {/* skills section */}
+      <Skills />
+      {/* Experience and Qualifications section */}
+      <Experience />
+      {/* projects section */}
+      <Projects />
+      {/* contact section and Footer*/}
 
-    {/* contact section */}
-    <Contact />
+      <Footer />
     </section>
   ) 
-  
 }
