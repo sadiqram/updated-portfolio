95be6810e83ec04e5d1018d6727bdbc5ca6f942e
Sadiq Ramanu <abdulrahmansadiq13@gmail.com>
2025-04-19 22:45:45 -0400

    fix build errors

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,16 +1,11 @@
 'use client';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { type ThemeProviderProps } from 'next-themes';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      {...props}
-    >
+    <NextThemesProvider {...props}>
       {children}
     </NextThemesProvider>
   );
